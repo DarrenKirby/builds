3af8b6065c4272fa7377d28da68d07c87ff375a9
Darren Kirby <bulliver@gmail.com>
2014-10-23 13:35:46 -0600

    Outsourced many functions from 'bld' to Build.pm'

--- scripts/Builds.pm
+++ scripts/Builds.pm
@@ -17,6 +17,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use strict;
+use warnings;
 
 package Builds;
 require Exporter;
@@ -25,7 +27,11 @@ our @ISA     = qw(Exporter);
 our @EXPORT  = qw(bold green yellow red $builds $distfiles $conf
                   $b $s $ub $us $ui $ul $ulb $uls $uli $ull do_dl
                   $man1 $man2 $man3 $man4 $man5 $man6 $man7 $man8
-                  $ins_bin $ins_scr $ins_lib $ins_hdr $ins_man);
+                  $ins_bin $ins_scr $ins_lib $ins_hdr $ins_man
+                  do_conf check_md5sum fetch_wrapper install_src_wrapper
+                  configure_wrapper make_wrapper make_install_wrapper
+                  sys_install_wrapper clean_wrapper get_tokens
+                  @valid_commands);
 
 
 # Useful variables used by all builds. These are
@@ -66,6 +72,11 @@ $ins_lib = "install -v -o root -g root -m 755";
 $ins_hdr = "install -v -o root -g root -m 644";
 $ins_man = "install -v -o root -g root -m 644";
 
+# Valid commands
+@valid_commands = qw/install fetch install_src configure 
+                     make make_install sys_install clean/
+
+
 # Functions used globally
 #
 
@@ -94,15 +105,215 @@ sub red {
     system('echo -en $"\\033[0;39m"');
 }
 
+# Download distfiles
 sub do_dl {
-    if (-e "$distfiles/$_[0]") {
+    if (-e "$distfiles/" . $_[0]) {
         bold "Nothing to fetch...\n";
     } else {
         chdir $distfiles;
-        unless (system("wget $_[0]") == 0) {
+        unless (system("wget " . $_[0]) == 0) {
             red "fetch failed\n";
             die;
         }
     }
 }
 
+# Check for a config file and parse it if it exists
+sub do_conf {
+    if (-e $conf) {
+        require $conf;
+    }
+
+    if (defined $cflags) {
+        $ENV{'CFLAGS'} = $cflags;
+    }
+    if (defined $cxxflags) {
+        $ENV{'CXXFLAGS'} = $cxxflags;
+    }
+}
+
+# Make sure downloaded tarballs match what we expect.
+sub check_md5sum {
+    my @array = split /\s+/, `md5sum $distfiles/$PN`;
+    if ($md5sum ne $array[0]) {
+        red "md5sums do not match\n";
+        die;
+    }
+    green "md5sums match ;-)\n";
+    return 0;
+}
+
+# These wrapper functions define the default action for each step.
+# Defaults can generally be used if the software follows the usual
+# './configure, make, make install' three-step. If special rules or
+# commands are needed to build a packages, you can define the non-
+# wrapper equivalent in the *.build file and those sub routines will
+# replace the default actions. This is a binary operation: either
+# the commands in the .build file will run, or the defaults defined
+# here will, not a mix of both.
+
+sub fetch_wrapper {
+    green "fetching files...\n";
+
+    if (defined &fetch) {
+        unless (&fetch() == 0) {
+            red "fetch failed!\n";
+            die;
+        }
+    } else {
+        do_dl($src_url);
+    }
+
+    chdir $here;
+    check_md5sum();
+    green "fetch complete\n";
+}
+
+sub install_src_wrapper {
+    green "installing source...\n";
+
+    if (defined &install_src) {
+        unless (&install_src() == 0) {
+            red "source install failed!\n";
+            die;
+        }
+    } else {
+        mkdir "build";
+        chdir "build";
+        unless (system("tar xf $distfiles/$PN -C .") == 0) {
+            red "source install failed!\n";
+            die;
+        }
+    }
+
+    green "source installed\n";
+}
+
+sub configure_wrapper {
+    green "running configure...\n";
+    chdir $here . "/build/" . $PD;
+
+    if (defined &configure) {
+        unless (&configure() == 0) {
+            red "configure failed!\n";
+            die;
+        }
+    } else {
+        unless (system("./configure --prefix=$here/build $CONFIG_OPTS") == 0) {
+            red "configure failed!\n";
+            die;
+        }
+    }
+
+    green "configure complete\n";
+}
+
+sub make_wrapper {
+    green "running make...\n";
+    chdir $here . "/build/" . $PD;
+
+    if (defined &make) {
+        unless (&make() == 0) {
+            red "make failed!\n";
+            die;
+        }
+    } else {
+        unless (system("make $MAKEOPTS") == 0) {
+            red "make failed!\n";
+            die;
+        }
+    }
+
+    green "make complete\n";
+}
+
+sub make_install_wrapper {
+    green "installing to temporary root...\n";
+    chdir $here . "/build/" . $PD;
+
+    if (defined &make_install) {
+        unless (&make_install() == 0) {
+            red "make install failed!\n";
+            die;
+        }
+    } else {
+        unless (system("make install") == 0) {
+            red "make install failed!";
+            die;
+        }
+    }
+}
+
+# This sub MUST be defined in the build script.
+sub sys_install_wrapper {
+    green "installing to system...\n";
+    chdir $here . "/build/";
+
+    unless (&sys_install() == 0) {
+        red "install failed!\n";
+        die;
+    }
+    green "installed files:\n";
+    @files = sort(@files);
+    foreach my $file (@files) {
+        if (-l $file) {
+            bold "[L] $file\n";
+        } elsif (-d $file) {
+            bold "[D] $file\n";
+        } else {
+            bold "\t$file\n";
+        }
+    }
+    green "install complete\n";
+}
+
+sub clean_wrapper {
+    green "cleaning up...\n";
+
+    if (defined &clean) {
+        unless (&clean() == 0) {
+            red "clean failed!\n";
+            die;
+        }
+    } else {
+        chdir $here;
+        unless (system("rm -rf build") == 0) {
+            yellow "clean failed, please remove build directory manually\n";
+        }
+    }
+
+    green "   done.\n";
+
+    bold "regenerating ld.so.cache...\n";
+    unless (system("ldconfig") == 0) {
+        yellow "ldconfig failed\n";
+        yellow "Package $PD installed, but with issues\n";
+    }
+}
+
+sub do_all {
+    &fetch_wrapper();
+    &install_src_wrapper();
+    &configure_wrapper();
+    &make_wrapper();
+    &make_install_wrapper();
+    &sys_install_wrapper();
+    &clean_wrapper();
+}
+
+# This subroutine creates some useful automatic variables.
+#   Given 'tar-1.28.build'' and 'tar-1.28.tar.xz':
+#   $N  => 'tar'
+#   $V  => '1.28'
+#   $PD => 'tar-1.28'
+#
+sub get_tokens {
+    my @arr = split /-/, $build;
+    # 'tar-1.28.build' => 'tar', '1.28.build'
+    $N = $arr[0];
+    my @arr2 = split /\./, $arr[1];
+    # '1.28.build' => '1', '28', 'build'
+    pop @arr2; # Discard 'build'
+    $V = join ".", @arr2;
+    $PD = join "-", ($N, $V);
+}
