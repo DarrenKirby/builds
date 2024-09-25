66a1d2f85b9c8d3c1154f2d1553f13c4837ffd80
Darren Kirby <bulliver@gmail.com>
2024-09-25 16:37:21 -0700

    First commit. Functions just roughed in...

---
+++ scripts/search_package.py
@@ -0,0 +1,27 @@
+#    /usr/builds/scripts/search_package.py
+#    Wed Sep 25 23:30:16 UTC 2024
+
+#    Core functionality of the bld command
+#
+#    Copyright:: (c) 2024 Darren Kirby
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+def do_search(args, config):
+    pass
+
+def do_info(args, config):
+    pass
+
