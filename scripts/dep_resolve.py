6b9dfb68d02f53e9975936324f1e74b0f0f18525
Darren Kirby <bulliver@gmail.com>
2024-10-01 20:03:00 -0700

    Edit to have pkg_to_build return a list of tuples including version string

--- scripts/dep_resolve.py
+++ scripts/dep_resolve.py
@@ -1,7 +1,7 @@
 #    /usr/builds/scripts/dep_resolve.py
 #    Mon Sep 30 02:10:47 UTC 2024
 
-#    Naïve dependancy resolver
+#    Naïve dependency resolver
 #
 #    Copyright:: (c) 2024 Darren Kirby
 #    Author:: Darren Kirby (mailto:bulliver@gmail.com)
@@ -34,36 +34,43 @@ def resolve_dependencies(args: list, config: dict) -> list:
     with dbm.open(config['db_file']) as db:
         for arg in args.pkg_atom:
             if arg.find('/') != -1:
-                a = db[arg.split('/')[1]].decode()
-                a = a.split(',')[0]
+                db_string = db[arg.split('/')[1]].decode()
+                pkg_name = db_string.split(',')[0]
                 # atom not in db...
-                if not a == arg:
+                if not pkg_name == arg:
                     cf.red(f"{arg} does not appear to be a valid package atom.")
                     cf.yellow(f"Try: 'bld search {arg}'")
                     sys.exit(2)
-                atoms.append(arg)
+                #atoms.append(arg)
             else:
                 try:
-                    a = db[arg].decode()
-                    atoms.append(a.split(',')[0])
+                    db_string = db[arg].decode()
+                    #atoms.append(a.split(',')[0])
                 except IndexError:
                     cf.red(f"'{arg}' does not appear to be a valid package name.")
                     cf.yellow(f"Try: 'bld search {arg}'")
                     sys.exit(2)
+            # Append tuple of atom and version
+            db_list = db_string.split(',')
+            atoms.append((db_list[0], db_list[1]))
+
     # This is a _REALLY_ naive implementation of a dependancy tree
     # This will have to be refactored and improved when the rest
     # of the scaffolding is up...
     for atom in atoms:
-        build_file = glob.glob(f"{config['builds_root']}/{atom}/*.build")[0]
+        build_file = glob.glob(f"{config['builds_root']}/{atom[0]}/*.build.py")[0]
         with open(build_file, 'r', encoding='UTF8') as fh:
             lines = fh.readlines()
             for line in lines:
                 if line.startswith('depend'):
                     pkgs = line.split('=')[-1]
                     for pkg in pkgs.split(','):
-                        # We do not yet distinguish between build depends and run depends,
-                        # so for now we just insert dependencies at the begining of the
-                        # list so that they get built first.
-                        pkgs_to_build.insert(0, pkg)
+                        with dbm.open(config['db_file']) as db:
+                            db_string = db[arg.split('/')[1]].decode()
+
+                            # We do not yet distinguish between build depends and run depends,
+                            # so for now we just insert dependencies at the begining of the
+                            # list so that they get built first.
+                            pkgs_to_build.insert(0, (pkg, db_string.split()[1]))
     pkgs_to_build += atoms
     return pkgs_to_build
