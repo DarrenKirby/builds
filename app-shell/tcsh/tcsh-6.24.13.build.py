f21d6b53d405c810316a7dd1d68a438111ad6241
Darren Kirby <bulliver@gmail.com>
2024-10-18 20:52:50 -0700

    tcsh build script complete

--- app-shell/tcsh/tcsh-6.24.13.build.py
+++ app-shell/tcsh/tcsh-6.24.13.build.py
@@ -1,8 +1,8 @@
-#    <category>/<name>/<name>.build
-#    `date --utc`
+#    app-shell/tcsh/tcsh-6.24.13.build.py
+#    Fri Oct 18 22:15:18 UTC 2024
 
-#    Copyright:: (c) 2024 <name>
-#    Author:: <name> (mailto:<email>)
+#    Copyright:: (c) 2024 Darren Kirby
+#    Author:: Darren Kirby (mailto:Darren Kirby)
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -18,52 +18,20 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# If there are no dependencies then comment this line out,
-# otherwise, add all dependencies to this list as strings ie:
-# depend=['dev-lang/ruby', 'dev-editor/nano']
-# All 'system' packages are implicit dependencies, and do not
-# need to be listed here as they are already installed.
-depend = []
+def configure(self):
+    return os.system(f"./configure --prefix={self.seg_dir}")
 
+def make(self):
+    return os.system("make")
 
-# Use these two as pre/post hooks into the fetch process
-# def fetch_prehook(self):
-#     pass
-#
-# def fetch_posthook(self):
-#     pass
-
-
-# Use these two as pre/post hooks into the source-install process
-# def install_source_prehook(self):
-#     pass
-#
-# def install_source_posthook(self):
-#     pass
-
-
-# make_install MUST be defined in the build file.
-# Use the helper functions in common_functions.py
-# to install binaries, scripts, libraries, headers,
-# documentation (man pages), and to create symlinks.
 def make_install(self):
-    pass
+    return os.system("make install install.man")
 
+def install(self):
+    cf.do_bin(f"{self.seg_dir}/bin/tcsh", cf.paths['ub'])
+    cf.do_man(f"{self.seg_dir}share/man/man1/tcsh.1", cf.paths['man1'])
 
-# Use these two as pre/post hooks into the cleanup process
-# def fetch_prehook(self):
-#     pass
-#
-# def fetch_posthook(self):
-#     pass
-
-
-# Write each installed file one per line in the commented section below.
-# This is the list that `bld uninstall` uses to know which files to remove.
 """
-/etc/foo.conf
-/usr/bin/foo
-/usr/lib/libfoo.so
-/usr/lib/libfoo.so.5.2
-/usr/share/man/man1/foo.1
+/usr/bin/tcsh
+/usr/share/man/man1/tcsh.1.bz2
 """
