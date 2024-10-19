cca14ca0af985b1a1de876c501cfbf943f72c6c4
Darren Kirby <bulliver@gmail.com>
2024-10-19 16:05:43 -0700

    bash build file complete

--- app-shell/bash/bash-5.2.37.build.py
+++ app-shell/bash/bash-5.2.37.build.py
@@ -1,8 +1,8 @@
-#    <category>/<name>/<name>.build
-#    `date --utc`
+#    app-shell/bash/bash-5.2.37.build.py
+#    Sat Oct 19 21:11:53 UTC 2024
 
-#    Copyright:: (c) 2024 <name>
-#    Author:: <name> (mailto:<email>)
+#    Copyright:: (c) 2024 Darren Kirby
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -18,52 +18,148 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# If there are no dependencies then comment this line out,
-# otherwise, add all dependencies to this list as strings ie:
-# depend=['dev-lang/ruby', 'dev-editor/nano']
-# All 'system' packages are implicit dependencies, and do not
-# need to be listed here as they are already installed.
-depend = []
+def configure(self):
+    return os.system(f"./configure --prefix={self.seg_dir} "
+                     f"--without-bash-malloc "
+                     f"--with-installed-readline "
+                     f"bash_cv_strtold_broken=no")
 
 
-# Use these two as pre/post hooks into the fetch process
-# def fetch_prehook(self):
-#     pass
-#
-# def fetch_posthook(self):
-#     pass
+def make(self):
+    return os.system("make")
 
 
-# Use these two as pre/post hooks into the source-install process
-# def install_source_prehook(self):
-#     pass
-#
-# def install_source_posthook(self):
-#     pass
-
-
-# make_install MUST be defined in the build file.
-# Use the helper functions in common_functions.py
-# to install binaries, scripts, libraries, headers,
-# documentation (man pages), and to create symlinks.
 def make_install(self):
-    pass
+    return os.system("make install")
 
 
-# Use these two as pre/post hooks into the cleanup process
-# def fetch_prehook(self):
-#     pass
-#
-# def fetch_posthook(self):
-#     pass
+def install(self):
+    cf.do_bin(f"{self.seg_dir}/bin/bash", cf.paths['ub'])
+    cf.do_scr(f"{self.seg_dir}/bin/bashbug", cf.paths['ub'])
+
+    # headers
+    cf.do_dir(f"{self.seg_dir}/include/bash/", f"{cf.paths['ui']}/bash/")
+    # builtins
+    cf.do_dir(f"{self.seg_dir}/lib/bash/", f"{cf.paths['ul']}/bash/")
+
+    cf.do_man(f"{self.seg_dir}/share/man/man1/bash.1", cf.paths['man1'])
+    cf.do_man(f"{self.seg_dir}/share/man/man1/bashbug.1", cf.paths['man1'])
 
 
-# Write each installed file one per line in the commented section below.
-# This is the list that `bld uninstall` uses to know which files to remove.
+def cleanup_posthook(self):
+    cf.yellow("Make /bin/sh link to /usr/bin/bash ? (y/n)")
+    if input() not in ['n', 'N', 'No', 'no']:
+        cf.do_sym("/usr/bin/bash", "/bin/sh")
+    cf.bold("Run...")
+    print("\texec /usr/bin/bash --login")
+    cf.bold("...to load new bash shell immediatly")
+
+
 """
-/etc/foo.conf
-/usr/bin/foo
-/usr/lib/libfoo.so
-/usr/lib/libfoo.so.5.2
-/usr/share/man/man1/foo.1
+/usr/bin/bash
+/usr/bin/bashbug
+/usr/include/bash/
+/usr/include/bash/alias.h
+/usr/include/bash/array.h
+/usr/include/bash/arrayfunc.h
+/usr/include/bash/assoc.h
+/usr/include/bash/bashansi.h
+/usr/include/bash/bashintl.h
+/usr/include/bash/bashjmp.h
+/usr/include/bash/bashtypes.h
+/usr/include/bash/builtins.h
+/usr/include/bash/command.h
+/usr/include/bash/config-bot.h
+/usr/include/bash/config-top.h
+/usr/include/bash/config.h
+/usr/include/bash/conftypes.h
+/usr/include/bash/dispose_cmd.h
+/usr/include/bash/error.h
+/usr/include/bash/execute_cmd.h
+/usr/include/bash/externs.h
+/usr/include/bash/general.h
+/usr/include/bash/hashlib.h
+/usr/include/bash/jobs.h
+/usr/include/bash/make_cmd.h
+/usr/include/bash/pathnames.h
+/usr/include/bash/quit.h
+/usr/include/bash/shell.h
+/usr/include/bash/sig.h
+/usr/include/bash/siglist.h
+/usr/include/bash/signames.h
+/usr/include/bash/subst.h
+/usr/include/bash/syntax.h
+/usr/include/bash/unwind_prot.h
+/usr/include/bash/variables.h
+/usr/include/bash/version.h
+/usr/include/bash/xmalloc.h
+/usr/include/bash/y.tab.h
+/usr/include/bash/builtins/
+/usr/include/bash/builtins/bashgetopt.h
+/usr/include/bash/builtins/builtext.h
+/usr/include/bash/builtins/common.h
+/usr/include/bash/builtins/getopt.h
+/usr/include/bash/include/
+/usr/include/bash/include/ansi_stdlib.h
+/usr/include/bash/include/chartypes.h
+/usr/include/bash/include/filecntl.h
+/usr/include/bash/include/gettext.h
+/usr/include/bash/include/maxpath.h
+/usr/include/bash/include/memalloc.h
+/usr/include/bash/include/ocache.h
+/usr/include/bash/include/posixdir.h
+/usr/include/bash/include/posixjmp.h
+/usr/include/bash/include/posixstat.h
+/usr/include/bash/include/posixtime.h
+/usr/include/bash/include/posixwait.h
+/usr/include/bash/include/shmbchar.h
+/usr/include/bash/include/shmbutil.h
+/usr/include/bash/include/shtty.h
+/usr/include/bash/include/stat-time.h
+/usr/include/bash/include/stdc.h
+/usr/include/bash/include/systimes.h
+/usr/include/bash/include/typemax.h
+/usr/include/bash/include/unionwait.h
+/usr/lib/bash/
+/usr/lib/bash/Makefile.inc
+/usr/lib/bash/Makefile.sample
+/usr/lib/bash/accept
+/usr/lib/bash/basename
+/usr/lib/bash/csv
+/usr/lib/bash/cut
+/usr/lib/bash/dirname
+/usr/lib/bash/dsv
+/usr/lib/bash/fdflags
+/usr/lib/bash/finfo
+/usr/lib/bash/getconf
+/usr/lib/bash/head
+/usr/lib/bash/id
+/usr/lib/bash/ln
+/usr/lib/bash/loadables.h
+/usr/lib/bash/logname
+/usr/lib/bash/mkdir
+/usr/lib/bash/mkfifo
+/usr/lib/bash/mktemp
+/usr/lib/bash/mypid
+/usr/lib/bash/pathchk
+/usr/lib/bash/print
+/usr/lib/bash/printenv
+/usr/lib/bash/push
+/usr/lib/bash/realpath
+/usr/lib/bash/rm
+/usr/lib/bash/rmdir
+/usr/lib/bash/seq
+/usr/lib/bash/setpgid
+/usr/lib/bash/sleep
+/usr/lib/bash/stat
+/usr/lib/bash/strftime
+/usr/lib/bash/sync
+/usr/lib/bash/tee
+/usr/lib/bash/truefalse
+/usr/lib/bash/tty
+/usr/lib/bash/uname
+/usr/lib/bash/unlink
+/usr/lib/bash/whoami
+/usr/share/man/man1/bash.1.bz2
+/usr/share/man/man1/bashbug.1.bz2
 """
