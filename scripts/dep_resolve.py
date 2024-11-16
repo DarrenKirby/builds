51e19086c0784bbf88969cabdd2988aea50ce41f
Darren Kirby <bulliver@gmail.com>
2024-11-15 20:35:46 -0800

    Implement topological sort for dependency resolution

--- scripts/dep_resolve.py
+++ scripts/dep_resolve.py
@@ -66,63 +66,114 @@ def process_packages(args: argparse.Namespace) -> list:
     return pkgs
 
 
+def get_deps(build_file: str) -> list:
+    """
+    Get dependancies for each package from build file.
+    """
+    deps = []
+    with open(build_file, 'r', encoding='utf-8') as fh:
+        lines = fh.readlines()
+        for line in lines:
+            if line.startswith('depend'):
+                pkgs = line.split('=')[-1].strip()
+                pkgs = pkgs[1:-1]  # remove quotes
+                for pkg in pkgs.split(','):
+                    if not already_installed(pkg):
+                        deps.append(pkg)
+    return deps
+
+
+def get_version(package: str) -> tuple[str, str]:
+    """
+    Return version number in db given a package.
+    """
+    with dbm.open(config['db_file']) as db:
+        if package.find('/') != -1:
+            db_string = db[package.split('/')[1]].decode()
+            pkg_name = db_string.split(',')[0]
+            # atom not in db...
+            if not pkg_name == package:
+                cf.red(f"{package} does not appear to be a valid package atom.")
+                cf.yellow(f"Try: 'bld search {package}'")
+                sys.exit(2)
+
+        else:
+            try:
+                db_string = db[package].decode()
+
+            except IndexError:
+                cf.red(f"'{package}' does not appear to be a valid package name.")
+                cf.yellow(f"Try: 'bld search {package}'")
+                sys.exit(2)
+        # Append tuple of atom and version
+        db_list = db_string.split(',')
+        return db_list[0], db_list[1]
+
+
+def topological_sort(graph: dict) -> list:
+    """
+    Perform topological sorting on a dependency graph.
+    Ensures that dependencies are built before the packages that depend on them.
+    """
+    from collections import defaultdict, deque
+
+    # Calculate in-degrees for each node
+    in_degree = defaultdict(int)
+    for node in graph:
+        for dep in graph[node]:
+            in_degree[dep] += 1
+
+    # Initialize the queue with nodes that have no incoming edges
+    queue = deque([node for node in graph if in_degree[node] == 0])
+    sorted_order = []
+
+    while queue:
+        current = queue.popleft()
+        sorted_order.append(current)
+
+        for dep in graph[current]:
+            in_degree[dep] -= 1
+            if in_degree[dep] == 0:
+                queue.append(dep)
+
+    # Check for cycles
+    if len(sorted_order) != len(graph):
+        raise ValueError("Circular dependency detected in dependency graph")
+
+    sorted_order.reverse()
+    return sorted_order
+
+
 def resolve_dependencies(args: argparse.Namespace) -> list:
     """
-    Translate package names into atoms, and check for dependencies
+    Resolve package dependencies, perform a topological sort, and
+    return a list of (expanded_name, version) tuples in build order.
     """
-    pkg_atoms = process_packages(args)
+    pkg_atoms = process_packages(args)  # Initial packages from the command line
+    version_dict = {}
+    dep_graph = {}
 
-    atoms = []
-    pkgs_to_build = []
-    with dbm.open(config['db_file']) as db:
-        for arg in pkg_atoms:
-            if arg.find('/') != -1:
-                db_string = db[arg.split('/')[1]].decode()
-                pkg_name = db_string.split(',')[0]
-                # atom not in db...
-                if not pkg_name == arg:
-                    cf.red(f"{arg} does not appear to be a valid package atom.")
-                    cf.yellow(f"Try: 'bld search {arg}'")
-                    sys.exit(2)
+    # Build the initial version dictionary and dependency graph
+    for pkg in pkg_atoms:
+        name, version = get_version(pkg)
+        version_dict[name] = version
+        dep_graph[name] = get_deps(f"{config['builds_root']}/{name}/{name.split('/')[-1]}-{version}.build.py")
 
-            else:
-                try:
-                    db_string = db[arg].decode()
+    # Recursively resolve dependencies
+    to_process = list(dep_graph.keys())  # Queue of packages to process
+    while to_process:
+        current = to_process.pop(0)
+        for dep in dep_graph[current]:
+            if dep not in dep_graph:  # If dependency hasn't been processed yet
+                dep_name, dep_version = get_version(dep)
+                version_dict[dep_name] = dep_version
+                dep_graph[dep_name] = get_deps(
+                    f"{config['builds_root']}/{dep_name}/{dep_name.split('/')[-1]}-{dep_version}.build.py"
+                )
+                to_process.append(dep_name)  # Add new dependency to processing queue
 
-                except IndexError:
-                    cf.red(f"'{arg}' does not appear to be a valid package name.")
-                    cf.yellow(f"Try: 'bld search {arg}'")
-                    sys.exit(2)
-            # Append tuple of atom and version
-            db_list = db_string.split(',')
-            atoms.append((db_list[0], db_list[1]))
+    # Perform topological sort
+    build_order = topological_sort(dep_graph)
 
-    # This is a _REALLY_ naive implementation of dependency resolution
-    # This will have to be refactored and improved when the rest
-    # of the scaffolding is up...
-    for atom in atoms:
-        build_file = glob.glob(f"{config['builds_root']}/{atom[0]}/*.build.py")[0]
-        with open(build_file, 'r', encoding='utf-8') as fh:
-            lines = fh.readlines()
-            for line in lines:
-                if line.startswith('depend'):
-                    pkgs = line.split('=')[-1].strip()
-                    pkgs = pkgs[1:-1]  # remove quotes
-                    for pkg in pkgs.split(','):
-                        if not already_installed(pkg):
-                            with dbm.open(config['db_file']) as db:
-                                if pkg.find('/') != -1:
-                                    db_string = db[pkg.split('/')[1]].decode()
-                                else:
-                                    db_string = db[pkg].decode()
-                                db_list = db_string.split(",")
-
-                                # We do not yet distinguish between build dependencies and run dependencies,
-                                # (let alone optional dependencies) so for now we just insert dependencies
-                                # at the beginning of the list so that they get built first.
-                                pkgs_to_build.insert(0, (db_list[0], db_list[1]))
-                        else:
-                            continue
-
-    pkgs_to_build += atoms
-    return pkgs_to_build
+    # Construct the final list of (name, version) tuples
+    return [(name, version_dict[name]) for name in build_order]
