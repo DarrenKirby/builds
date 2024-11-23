9527fcd9c2a62c1141a70f430e36c95366c6c3b8
Darren Kirby <bulliver@gmail.com>
2024-11-23 10:21:34 -0800

    gmp,mpc,mpfr build scripts ready for testing

---
+++ sci-math/gmp/gmp-6.3.0.build.py
@@ -0,0 +1,38 @@
+#    sci-math/gmp/gmp-6.3.0.build.py
+#    Sat Nov 23 17:44:07 UTC 2024
+
+#    Copyright:: (c)
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def configure(self):
+    return os.system("./configure --prefix=/usr --enable-cxx --disable-static")
+
+
+def make(self):
+    return os.system(f"make {cf.config['makeopts']}")
+
+
+def make_install(self):
+    return os.system(f"make DESTDIR={self.seg_dir} install")
+
+
+def install(self):
+    #self.inst_binary(f"{self.p['_ub']}/bc", self.p['ub'])
+    #self.inst_symlink(f"{self.p['ub']}/bc", f"{self.p['ub']}/dc")
+
+    #self.inst_manpage(f"{self.p['_man1']}/bc.1", self.p['man1'])
+    #self.inst_manpage(f"{self.p['_man1']}/dc.1", self.p['man1'])
