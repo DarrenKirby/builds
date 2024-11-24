06e8d0e47b0adf9e3b164d28de5fa2d49515140c
Darren Kirby <bulliver@gmail.com>
2024-11-23 19:11:36 -0800

    New build scripts ready for testing

---
+++ dev-tool/gettext/gettext-0.22.5.build.py
@@ -0,0 +1,34 @@
+#    dev-tool/gettext/gettext-0.22.5.build.py
+#    Sun Nov 24 03:05:23 UTC 2024
+
+#    Copyright:: (c) 2024
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def configure(self):
+    return os.system("../configure --prefix=/usr --disable-static")
+
+
+def make(self):
+    return os.system(f"make {cf.config['makeopts']}")
+
+
+def make_install(self):
+    return os.system(f"make DESTDIR={self.seg_dir} install")
+
+
+def install(self):
+    pass
