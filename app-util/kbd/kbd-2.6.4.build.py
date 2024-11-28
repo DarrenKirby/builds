c372cb3544d5a41ab01579002fea243e2fcc268d
Darren Kirby <bulliver@gmail.com>
2024-11-28 14:58:08 -0800

    Heap of fresh, new build files

---
+++ app-util/kbd/kbd-2.6.4.build.py
@@ -0,0 +1,35 @@
+#    app-util/kbd/kbd-2.6.4.build.py
+#    Thu Nov 21 16:33:12 UTC 2024
+
+#    Copyright:: (c) 2024
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def configure(self):
+    # Prevent static libs
+    return self.do("sed -i '/install -m.*STA/d' libcap/Makefile")
+
+
+def make(self):
+    return self.do(f"make {cf.config['makeopts']} prefix={self.seg_dir}/usr lib=lib")
+
+
+def make_install(self):
+    return self.do(f"make prefix={self.seg_dir}/usr lib=lib install")
+
+
+def install(self):
+    pass
---
+++ net-util/iproute2/iproute2-6.11.0.build.py
@@ -0,0 +1,36 @@
+#    net-util/iproute2/iproute2-6.11.0.build.py
+#    Sat Nov 16 21:11:57 UTC 2024
+
+#    Copyright:: (c) 2024
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def configure(self):
+    return self.do(f"./configure --prefix=/usr --with-ssl=openssl")
+
+
+def make(self):
+    return self.do(f"make {cf.config['makeopts']}")
+
+
+def make_install(self):
+    return self.do(f"make DESTDIR={self.seg_dir} install")
+
+
+def install(self):
+    self.inst_binary(f"{self.p['_ub']}/wget", self.p['ub'])
+    self.inst_manpage(f"{self.p['_man1']}/wget.1", self.p['man1'])
+    self.inst_config(f"{self.p['_ue']}/wgetrc", self.p['e'] if cf.config['user'] == 'root' else self.p['ue'])
