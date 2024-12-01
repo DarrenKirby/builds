9f856a3fcc4bc8f049c995b854ccf0924abb77db
Darren Kirby <bulliver@gmail.com>
2024-12-01 13:06:34 -0800

    Build file done and tested

---
+++ app-www/links/links-2.30.build.py
@@ -0,0 +1,37 @@
+#    app-www/links/links-2.30.build.py
+#    Sun Dec  1 19:20:47 UTC 2024
+
+#    Copyright:: (c) 2024
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+depend = "lib-util/libevent/"
+
+def configure(self):
+    return self.do("./configure --prefix=/usr --mandir=/usr/share/man")
+
+
+def make(self):
+    return self.do(f"make {cf.config['makeopts']}")
+
+
+def make_install(self):
+    return self.do(f"make DESTDIR={self.seg_dir} install")
+
+
+def install(self):
+    self.inst_binary(self.p['_ub'] + "/links", self.p['ub'])
+
+    self.inst_manpage(self.p['_man1'] + "/links.1", self.p['man1'])
