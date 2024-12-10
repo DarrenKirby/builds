7262b92ba6ed6584aa291ca09c9ed3ec4be82e89
Darren Kirby <bulliver@gmail.com>
2024-12-10 15:53:16 -0800

    Roughed in, needs to be added to db

---
+++ dev-util/check/check-0.15.2.build.py
@@ -0,0 +1,35 @@
+#    dev-util/check/check-0.15.2.build.py
+#    Tue Dec 10 23:45:57 UTC 2024
+
+#    Copyright:: (c) 2024
+#    Author:: Darren Kirby (mailto:bulliver@gmail.com)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+def configure(self):
+    return self.do("./configure --prefix=/usr")
+
+
+def make(self):
+    return self.do(f"make {cf.config['makeopts']}")
+
+
+def make_install(self):
+    return self.do(f"make DESTDIR={self.seg_dir} install")
+
+
+def install(self):
+    pass
